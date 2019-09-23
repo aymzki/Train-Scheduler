c1e2c355db102fdc710d52a77e7039c5a28745df
Alissa Yamazaki <alissa.yamazaki@gmail.com>
2019-09-23 12:06:46 -0400

    grab user inputs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,24 @@
 //Initialize Firebase
 var firebaseConfig = {
-    apiKey: "AIzaSyBZeXAGfmNC6pWFa_cvgTw7ccft186RaGw",
-    authDomain: "train-scheduler-cfe6d.firebaseapp.com",
-    databaseURL: "https://train-scheduler-cfe6d.firebaseio.com",
-    projectId: "train-scheduler-cfe6d",
-    storageBucket: "",
-    messagingSenderId: "461375329203",
-    appId: "1:461375329203:web:4dd106b6486187101b71c6"
-  };
-  
-  firebase.initializeApp(firebaseConfig);
+  apiKey: "AIzaSyBZeXAGfmNC6pWFa_cvgTw7ccft186RaGw",
+  authDomain: "train-scheduler-cfe6d.firebaseapp.com",
+  databaseURL: "https://train-scheduler-cfe6d.firebaseio.com",
+  projectId: "train-scheduler-cfe6d",
+  storageBucket: "",
+  messagingSenderId: "461375329203",
+  appId: "1:461375329203:web:4dd106b6486187101b71c6"
+};
 
-  //Reference to database
-  var database = firebase.database();
+firebase.initializeApp(firebaseConfig);
 
-  
-// var trainName
-// var whereTo 
-// var firstTime -- in military time
-// var freq -- in minutes
+//Reference to database
+var database = firebase.database();
+
+
+// Grab user inputs
+var trainName = $("name-input").val().trim();
+var whereTo = $("destination-input").val().trim();
+var firstTime = moment($("firsttime-input").val().trim(),"HH:mm").format("LT");
+var freq = $("freq-input").val().trim();
+
+//
