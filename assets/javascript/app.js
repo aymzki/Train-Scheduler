65c91fec2cfe7f39807014bd1302e2f84ca5458f
Alissa Yamazaki <alissa.yamazaki@gmail.com>
2019-09-23 14:01:10 -0400

    upload input to db and create firebase event for adding info to db

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,11 +14,64 @@ firebase.initializeApp(firebaseConfig);
 //Reference to database
 var database = firebase.database();
 
+//Submit Button to add trains
+$("#submit-btn").on("click", function(event) {
+  event.preventDefault();
 
-// Grab user inputs
-var trainName = $("name-input").val().trim();
-var whereTo = $("destination-input").val().trim();
-var firstTime = moment($("firsttime-input").val().trim(),"HH:mm").format("LT");
-var freq = $("freq-input").val().trim();
 
-//
+  // Grab user inputs
+  var trainName = $("name-input").val().trim();
+  var whereTo = $("destination-input").val().trim();
+  var firstTime = moment($("firsttime-input").val().trim(), "HH:mm").format("LT");
+  var freq = $("freq-input").val().trim();
+
+  //Create local temporary object for all this data
+  var newTrain = {
+    name: trainName,
+    where: whereTo,
+    time: firstTime,
+    frequency: freq,
+  };
+
+  //Upload it to database (this is to local db or firebase db?)
+  database.ref().push(newTrain);
+
+  //Log everything to console
+  console.log(newTrain.name);
+  console.log(newTrain.where);
+  console.log(newTrain.time);
+  console.log(newTrain.frequency);
+
+  alert("Train successfully added");
+
+  // Clears all text boxes
+  $("#name-input").val("");
+  $("#destination-input").val("");
+  $("#firstTime-input").val("");
+  $("#freq-input").val("");
+
+});
+
+//Create Firebase event for adding train info to database
+//Also add row in html when there is data input
+
+database.ref().on("child_added", function(childSnapshot) {
+  console.log(childSnapshot.val());
+
+  //Store everything into a variable
+  var trainName = childSnapshot.val().name;
+  var whereTo = childSnapshot.val().where;
+  var firstTime = childSnapshot.val().time;
+  var freq = childSnapshot.val().frequency;
+
+  //Train Info
+  
+  console.log(trainName);
+  console.log(whereTo);
+  console.log(firstTime);
+  console.log(freq);
+
+  //Clean up the time
+  //Create new row
+  //Append new row to the table
+});
